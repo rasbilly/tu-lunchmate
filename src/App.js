3011f4178b6a83a9df4b26d4cf046a5960787ed2
Tobias Dunker <dunker@campus.tu-berlin.de>
2019-05-23 11:28:18 +0200

    Behebung der Anmerkungen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './App.css';
