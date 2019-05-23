78459d5d9344f243c770f8f12916ce6fc01d608f
Denis Koljada <denis@koljada.de>
2019-05-23 22:19:22 +0200

    first firebase implementation with basic auth functions

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,30 @@
+import app from 'firebase/app';
+import 'firebase/auth'
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DBURL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
+};
+
+class Firebase {
+    constructor() {
+        console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+        app.initializeApp(config);
+        this.auth = app.auth
+    }
+
+    //Auth functions
+    createUser = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+    signIn = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+    signOut = () => this.auth.signOut();
+    resetPassword = email => this.auth.sendPasswordResetEmail(email);
+    updatePassword = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
